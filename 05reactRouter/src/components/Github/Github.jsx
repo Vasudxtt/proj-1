7edf0e23ee454674a5320f5b686d7a6e7efe71d1
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-11 01:15:43 +0530

    added image with api

--- 05reactRouter/src/components/Github/Github.jsx
+++ 05reactRouter/src/components/Github/Github.jsx
@@ -10,7 +10,9 @@ function Github() {
     })
   },[])
   return (
-    <div className='text-center m-4 bg-green-600 text-white p-4 text-5xl'>Github followers : {data.followers} </div>
+    <div className='text-center m-4 bg-green-600 text-white p-4 text-5xl'>Github followers : {data.followers}
+    <img src={data.avatar_url} alt='Git picture' width={300}/>
+    </div>
   )
 }
 
