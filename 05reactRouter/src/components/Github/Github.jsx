d0fec5a83609c747c7dc543aa441531d15aeddae
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-11 01:27:02 +0530

    used Loader concepts

--- 05reactRouter/src/components/Github/Github.jsx
+++ 05reactRouter/src/components/Github/Github.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react'
-
+import { useLoaderData } from 'react-router-dom'
 function Github() {
-  const [data,setData] = useState([])
-  useEffect(()=>{
-    fetch('https://api.github.com/users/Vasudxtt').then(response =>response.json())
-    .then(data =>{
-      console.log(data);
-      setData(data)
-    })
-  },[])
+  const data = useLoaderData()
+  // const [data,setData] = useState([])
+  // useEffect(()=>{
+  //   fetch('https://api.github.com/users/Vasudxtt').then(response =>response.json())
+  //   .then(data =>{
+  //     console.log(data);
+  //     setData(data)
+  //   })
+  // },[])
   return (
     <div className='text-center m-4 bg-green-600 text-white p-4 text-5xl'>Github followers : {data.followers}
     <img src={data.avatar_url} alt='Git picture' width={300}/>
@@ -17,3 +18,9 @@ function Github() {
 }
 
 export default Github
+
+
+export const githubInfoLoader = async() =>{
+  const response = await fetch ('https://api.github.com/users/Vasudxtt')
+  return response.json()
+}
