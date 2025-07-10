09f2f6de1c0374e6fcd0f3d3ace7e49d3006d519
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-11 00:07:41 +0530

    changed

--- 05reactRouter/src/App.jsx
+++ 05reactRouter/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className='bg-green-600
-    p-4'>REact ROuter</h1>
+   <>
+   <Header/>
+   <Home/>
+   <Footer/>
+   </>
   )
 }
 
