0dd6b05eda31a81553d7ad3a9f66e183d67004c0
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-12 03:40:10 +0530

    update

--- 08todo/src/App.jsx
+++ 08todo/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Todoprovider} from'./context'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {todos,setTodos} = useState([])
 
   return (
+    <Todoprovider>
     <div className="bg-[#172842] min-h-screen py-8">
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
         <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
@@ -17,7 +17,7 @@ function App() {
           {/*Loop and Add TodoItem here */}
         </div>
       </div>
-    </div>
+    </Todoprovider>
   )
 }
 
