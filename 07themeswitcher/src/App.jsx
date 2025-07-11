a9cf89ca7f24b96e53e444ecd8ca2330af37d016
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-12 02:10:27 +0530

    Added theme button

--- 07themeswitcher/src/App.jsx
+++ 07themeswitcher/src/App.jsx
@@ -1,25 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider,useTheme } from './context/Theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const[themeMode,setThemeMode] = useState('Light')
+
+const lightMode=()=>{
+  setThemeMode('Light')
+}
+
+const darkMode=()=>{
+  setThemeMode('Dark')
+}
+
+// actual chnage in theme
+
+useEffect(()=>{
+  document.querySelector('html').classList.remove("light","dark")
+  document.querySelector('html').classList.add(themeMode)
+},[themeMode])
+
 
   return (
-
+    <ThemeProvider value={{themeMode,darkMode,lightMode}}>
     <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
 
         </div>
-
+{/* theme btn*/ }
         <div className="w-full max-w-sm mx-auto">
-
+{/* theme card*/ }
         </div>
       </div>
     </div>
-
+</ThemeProvider>
   )
 }
 
